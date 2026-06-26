930d04d26f8a3ac66ba0c58ef7ab37ccd17d3525
김도현 <kimdoes2143@naver.com>
2026-06-26 15:19:17 +0900

    test

--- src/test/java/com/ceos23/spring_boot/auth/controller/AuthControllerTest.java
+++ src/test/java/com/ceos23/spring_boot/auth/controller/AuthControllerTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("ci")
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
