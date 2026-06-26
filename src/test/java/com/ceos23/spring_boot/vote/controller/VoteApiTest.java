cc4d7b41787787018b59e8da19224be72fe07acf
김도현 <kimdoes2143@naver.com>
2026-06-26 17:16:53 +0900

    fix: CD/CD과정 수정

--- src/test/java/com/ceos23/spring_boot/vote/controller/VoteApiTest.java
+++ src/test/java/com/ceos23/spring_boot/vote/controller/VoteApiTest.java
@@ -34,7 +34,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
+@ActiveProfiles("ci")
 class VoteApiTest {
 
     @Autowired
